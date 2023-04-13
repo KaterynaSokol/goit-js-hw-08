a8303a3e17af4067932ae4166437525048028181
KaterynaSokol <k.sokol2019@gmail.com>
2023-04-13 21:48:57 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe, { id: 19231868, width: 640 });
+
+player.on(
+  'timeupdate',
+  throttle(function (data) {
+    let time = data.seconds;
+    console.log(data);
+    try {
+      localStorage.setItem('videoplayer-time', timing);
+    } catch (error) {
+      console.log(error.name);
+    }
+  }, 1000)
+);
+
+if (localStorage.getItem('videoplayer-time')) {
+  player.setCurrentTime(JSON.parse(localStorage.getItem('videoplayer-time')));
+}
