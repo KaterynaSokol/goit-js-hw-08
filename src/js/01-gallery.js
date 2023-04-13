0cccc0ab5169184505a7bfe081f669521ae62334
KaterynaSokol <k.sokol2019@gmail.com>
2023-04-13 18:31:56 +0300

    T1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+const galleryMarkup = createGalleryItems(galleryItems);
+
+function createGalleryItems(galleryItems) {
+  return galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt=${description} />
+   </a>
+</li>`
+    )
+    .join('');
+}
+gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+
+const galleryModal = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
