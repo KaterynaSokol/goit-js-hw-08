831bafe0d887a87edd056d7fe6e48c0b8d117e0a
KaterynaSokol <k.sokol2019@gmail.com>
2023-04-14 20:40:34 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,10 +1,10 @@
 import throttle from 'lodash.throttle';
 
-const form = document.querySelector('.feedback-form');
+
 
 const emailInput = form.querySelector('input[name="email"]');
 const messageInput = form.querySelector('textarea[name="message"]');
-
+const form = document.querySelector('.feedback-form');
 const objects = {};
 
 form.addEventListener('input', throttle(onFormInput, 500));
